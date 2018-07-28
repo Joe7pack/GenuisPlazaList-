4e4502e32c1e25b79139165e1695398321558499
Joe <Joe@Guzzardo.com>
2018-07-28 14:01:06 -0500

    correct toolbar on add new contact

--- GenuisPlazaList/app/src/main/java/com/guzzardo/genuisplazalist/AddEmployeeActivity.java
+++ GenuisPlazaList/app/src/main/java/com/guzzardo/genuisplazalist/AddEmployeeActivity.java
@@ -3,6 +3,7 @@ package com.guzzardo.genuisplazalist;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class AddEmployeeActivity extends AppCompatActivity {
 
@@ -13,6 +14,12 @@ public class AddEmployeeActivity extends AppCompatActivity {
         mResources = getResources();
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_employee_detail);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.add_employee_tool_bar);
+        setSupportActionBar(toolbar);
+
     }
 
 }
